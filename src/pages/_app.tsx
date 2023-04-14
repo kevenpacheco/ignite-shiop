febebf518f2b3541b813064c2ec14f383e50330c
Keven Pacheco <kevenpacheco2000@gmail.com>
2023-04-14 18:52:18 -0300

    Finalize project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+import logoImg from "@src/assets/logo.svg";
+import { Container, Header } from "@src/styles/pages/app";
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,85 @@
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
+import Stripe from "stripe";
+
+import { stripe } from "@src/lib/stripe";
+import { HomeContainer, Product } from "@src/styles/pages/home";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  }[];
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.id}`}
+              prefetch={false}
+              className="keen-slider__slide"
+            >
+              <Product>
+                <Image src={product.imageUrl} alt="" width={520} height={480} />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </HomeContainer>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      }).format(price.unit_amount ? price.unit_amount / 100 : 0),
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
+  };
+};
